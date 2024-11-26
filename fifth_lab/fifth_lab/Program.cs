5ac45bafbf6eae31c1b9a71293980f4f2eaae2b0
Julia Ohorodnichuk <101930820+ohorodnichuk17@users.noreply.github.com>
2024-11-26 16:22:16 +0200

    Add five lab

---
+++ fifth_lab/fifth_lab/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace fifth_lab;
+
+class Program
+{
+    static void Main()
+    {
+        PolarCoordinates coordinates = new CylindricalCoordinates(5, Math.PI / 4, 10);
+        
+        coordinates.ConvertToCartesian(out double x, out double y);
+        
+        Console.WriteLine($"Декартові координати: x = {x}, y = {y}");
+    }
+}
